7849f920a8f56d166f11a99ab1ca612789827b13
Sanvith S Kotian <nn25cse300@nmamit.in>
2026-07-27 12:03:02 +0530

    feat: show only assigned patients in doctor dashboard

--- lib/screens/doctor/doctor_patients_screen.dart
+++ lib/screens/doctor/doctor_patients_screen.dart
@@ -1,21 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'doctor_patient_details_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class DoctorPatientsScreen extends StatelessWidget {
   const DoctorPatientsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-
+    final doctor = FirebaseAuth.instance.currentUser;
+    if (doctor == null) {
+  return const Scaffold(
+    body: Center(
+      child: Text("Please login first"),
+    ),
+  );
+}
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Patients"),
+        title: const Text("My Patients"),
       ),
       body: StreamBuilder<QuerySnapshot>(
   stream: FirebaseFirestore.instance
       .collection('users')
       .where('role', isEqualTo: 'patient')
+      .where(
+      'assignedDoctorId',
+      isEqualTo: doctor.uid,
+    )
       .snapshots(),
   builder: (context, snapshot) {
     if (snapshot.hasError) {
@@ -26,8 +38,37 @@ class DoctorPatientsScreen extends StatelessWidget {
 
     if (snapshot.connectionState == ConnectionState.waiting) {
       return const Center(
-        child: CircularProgressIndicator(),
-      );
+  child: Padding(
+    padding: EdgeInsets.all(24),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          Icons.people_outline,
+          size: 80,
+          color: Colors.grey,
+        ),
+        SizedBox(height: 20),
+        Text(
+          "No Assigned Patients",
+          style: TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(height: 10),
+        Text(
+          "Patients assigned to you will appear here after you approve their appointments.",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Colors.grey,
+            fontSize: 15,
+          ),
+        ),
+      ],
+    ),
+  ),
+);
     }
 
     final patients = snapshot.data!.docs;
